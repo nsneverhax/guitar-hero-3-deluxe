153c0ea953676299619d18a9a0adbf8161bcc4a6
Glitchgod <glitchgod010110@gmail.com>
2025-01-13 18:23:15 +0000

    fix linux build

--- linux_scripts/build_ps2.sh
+++ linux_scripts/build_ps2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cd ..
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-find "$SCRIPT_DIR/../build/ps2/deluxe" -type f -exec rm -f {} \;
-rm -rf "$SCRIPT_DIR/../build/ps2/deluxe"
+find "$SCRIPT_DIR/build/ps2/deluxe" -type f -exec rm -f {} \;
+rm -rf "$SCRIPT_DIR/build/ps2/deluxe"
 echo "Building Guitar Hero 3 Deluxe files..."
-honeycomb pak compile "$SCRIPT_DIR/../_qb" -g gh3 -c ps2
-mkdir -p "$SCRIPT_DIR/../build/ps2/deluxe"
-mv "$SCRIPT_DIR/../_qb.pak.ps2" "$SCRIPT_DIR/../build/ps2/deluxe/gh3dx.pak.ps2"
+dependencies/linux/Honeycomb-CLI/Honeycomb pak compile "$SCRIPT_DIR/_qb" -g gh3 -c ps2
+mkdir -p "$SCRIPT_DIR/build/ps2/deluxe"
+mv "$SCRIPT_DIR/_qb.pak.ps2" "$SCRIPT_DIR/build/ps2/deluxe/gh3dx.pak.ps2"
 echo "Created Guitar Hero 3 Deluxe files."
 echo "Complete! Copy the contents of the ps2 folder in build into your GH3 extracted WAD folder."
